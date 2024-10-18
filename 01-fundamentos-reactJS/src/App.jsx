4f57211afc7253916c10368501ac452c20761efb
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-17 22:19:43 -0400

    import and export methods

--- 01-fundamentos-reactJS/src/App.jsx
+++ 01-fundamentos-reactJS/src/App.jsx
@@ -1,9 +1,13 @@
+import { Post } from "./components/post"
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <div>
+        <h1>Hello World!</h1>
+        <Post/>
+      </div>
     </>
   )
 
