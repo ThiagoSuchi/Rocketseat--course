be4c869dc531735fc2279b61561bbb529e7fa0fd
THIAGO <thiagohenssuchi@gmail.com>
2024-10-21 18:18:59 -0400

    building the header and sidebar of my feed

--- 01-fundamentos-reactJS/src/App.jsx
+++ 01-fundamentos-reactJS/src/App.jsx
@@ -1,22 +1,32 @@
 // components
 import { Header } from "./components/Header"
 import { Post } from "./post"
+import { Sidebar } from "./components/Sidebar"
 // styles
-import './styles.css'
+import styles from './App.module.css'
+import './global.css'
 
 function App() {
 
   return (
     <div>
-      <Header/>
-      <h1>Hello World!</h1>
-      <Post
-        author="Thiago Hens"
-        content="Duis amet nulla tempor laboris culpa. Reprehenderit exercitation excepteur do esse consectetur cupidatat minim. Minim culpa culpa eiusmod adipisicing culpa excepteur consectetur eu quis Lorem voluptate."
-      />
-      <Post 
-      author="Letícia Almeida" 
-      content="Sunt do eiusmod amet laborum sunt proident veniam quis commodo duis reprehenderit duis. Labore reprehenderit officia do qui anim ex ullamco nostrud elit enim proident nulla minim do. Eu sit eu veniam minim in sunt sunt irure velit. Amet proident reprehenderit laboris pariatur culpa laborum. Anim laborum adipisicing incididunt Lorem minim et consequat labore. Ut qui enim duis qui elit Lorem. Nulla ut in sunt ea laborum do magna anim."/>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <h1>Hello World!</h1>
+
+          <Post
+            author="Thiago Hens"
+            content="Duis amet nulla tempor laboris culpa. Reprehenderit exercitation excepteur do esse consectetur cupidatat minim. Minim culpa culpa eiusmod adipisicing culpa excepteur consectetur eu quis Lorem voluptate."
+          />
+
+          <Post
+            author="Letícia Almeida"
+            content="Sunt do eiusmod amet laborum sunt proident veniam quis commodo duis reprehenderit duis. Labore reprehenderit officia do qui anim ex ullamco nostrud elit enim proident nulla minim do. Eu sit eu veniam minim in sunt sunt irure velit. Amet proident reprehenderit laboris pariatur culpa laborum. Anim laborum adipisicing incididunt Lorem minim et consequat labore. Ut qui enim duis qui elit Lorem. Nulla ut in sunt ea laborum do magna anim." /></main>
+      </div>
     </div>
   )
 
--- 01-fundamentos-reactJS/src/components/Header.jsx
+++ 01-fundamentos-reactJS/src/components/Header.jsx
@@ -1,9 +1,12 @@
+// Styless
 import styles from './Header.module.css'
+// Logo Safira
+import safiraLogo from '../assets/safira-logo.png'
 
 export function Header () {
     return (
         <header className={styles.header}>
-            <strong>Safira</strong>
+            <img src={safiraLogo} alt="logotipo-Safria" />
         </header>
     )
 }
