1a309d08ec7c00799dfc74ac233eee11f2d50602
THIAGO <thiagohenssuchi@gmail.com>
2024-10-23 16:02:57 -0400

    Sidebar finalizado

--- 01-fundamentos-reactJS/src/components/Sidebar.jsx
+++ 01-fundamentos-reactJS/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 // styless
 import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -7,12 +8,15 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1690585703267-de31ea667ef0?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         
         <div className={styles.profile}>
-            <strong>Thiago Hens</strong>
+            <img className={styles.avatar} src="https://github.com/ThiagoSuchi.png" alt="" />
+
+            <strong>Thiago Hens Suchi</strong>
             <span>Web Developer</span>
         </div>
 
         <footer>
             <a href="#">
+                <PencilLine size={20}/>
                 Editar seu perfil
             </a>
         </footer>
